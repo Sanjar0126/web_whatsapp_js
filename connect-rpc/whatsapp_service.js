7a8ddc6c37f2bc8f09bcd2c8d2001f38fb800fe1
Sanjar0126 <sanjar.niyozov.0126@gmail.com>
2025-11-26 17:49:59 +0500

    add grpc service

---
+++ connect-rpc/whatsapp_service.js
@@ -0,0 +1,55 @@
+import { WhatsappService } from "../genproto/whatsapp_service/whatsapp_service_pb.js";
+import { fastifyConnectPlugin } from "@connectrpc/connect-fastify";
+import { ConnectError, Code } from "@connectrpc/connect";
+import { obfuscateText } from '../helper/obfuscate.js';
+
+export default async function connectRoutes(fastify, options) {
+    fastify.register(fastifyConnectPlugin, {
+        routes(router) {
+            router.service(WhatsappService, {
+                async pushWhatsappMessage(request, context) {
+                    fastify.log.info(
+                        `Received PushWhatsappMessage: ${request.shipperId} ${request.phone}`
+                    );
+
+                    const client = fastify.whatsappManager.getClient(request.shipperId);
+                    if (!client) {
+                        fastify.log.error(`Client ${request.shipperId} not found`);
+                        throw new ConnectError(
+                            `Client ${request.shipperId} not found`,
+                            Code.NotFound
+                        );
+                    }
+
+                    const status = client.getStatus();
+                    if (!status.isReady) {
+                        fastify.log.error(`Client ${request.shipperId} is not ready`);
+                        throw new ConnectError(
+                            `Client ${request.shipperId} is not ready`,
+                            Code.FailedPrecondition
+                        );
+                    }
+
+                    const obfuscatedText = obfuscateText(request.text)
+
+                    try {
+                        client.sendMessage(request.phone, obfuscatedText, Math.random() * 10000)
+                            .catch(err => fastify.log.error(`Background send failed: ${err.message}`));
+
+                        return {};
+                    } catch (err) {
+                        fastify.log.error(`Failed to send message: ${err.message}`);
+
+                        throw new ConnectError(
+                            "Internal error while sending WhatsApp message",
+                            Code.Internal,
+                            undefined,
+                            err
+                        );
+                    }
+                }
+            })
+        }
+    });
+}
+
