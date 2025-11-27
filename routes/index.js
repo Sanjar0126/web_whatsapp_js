91e1f4aafbe70bb05141b69b077496b5c2660977
Sanjar0126 <sanjar.niyozov.0126@gmail.com>
2025-11-27 11:15:43 +0500

    turn off rest api route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import wwebAPIRoutes from './wweb.js';
 import connectRoutes from '../connect-rpc/whatsapp_service.js';
 
 export default async function routes(fastify, options) {
-    fastify.register(wwebAPIRoutes, { prefix: '/whatsapp' });
+    // fastify.register(wwebAPIRoutes, { prefix: '/whatsapp' });
 
     // Note: Connect handles its own routing paths (e.g. /whatsapp_service.WhatsappService/PushWhatsappMessage)
     fastify.register(connectRoutes);
